a62db4249ffcb2f41481ede41ff46690f182d16c
Kuroyukihane <tanimalhoughton@yahoo.ca>
2016-12-03 22:12:17 -0400

    new buttons for ai selection
    they are lovely

--- Tak/core/src/com/mygdx/game/Screens/Declerations.java
+++ Tak/core/src/com/mygdx/game/Screens/Declerations.java
@@ -317,10 +317,10 @@ public class Declerations
         versusAISkin[18].add("versusAITitle", new Texture(Gdx.files.internal("versusAI/titleAI.png")));
         versusAISkin[19].add("versusMessage", new Texture(Gdx.files.internal("versusScreen/versusMessage.png")));
         versusAISkin[20].add("difficultyMessage", new Texture(Gdx.files.internal("versusAI/difficultyMessage.png")));
-        versusAISkin[21].add("difficultyOneUnpressed", new Texture(Gdx.files.internal("versusAI/BlankButton.png")));
-        versusAISkin[22].add("difficultyOnePressed", new Texture(Gdx.files.internal("versusAI/BlankButton.png")));
-        versusAISkin[23].add("difficultyTwoUnpressed", new Texture(Gdx.files.internal("versusAI/BlankButton.png")));
-        versusAISkin[24].add("difficultyTwoPressed", new Texture(Gdx.files.internal("versusAI/BlankButton.png")));
+        versusAISkin[21].add("difficultyOneUnpressed", new Texture(Gdx.files.internal("versusAI/AIButton1.png")));
+        versusAISkin[22].add("difficultyOnePressed", new Texture(Gdx.files.internal("versusAI/AIButton1selected.png")));
+        versusAISkin[23].add("difficultyTwoUnpressed", new Texture(Gdx.files.internal("versusAI/AIButton2.png")));
+        versusAISkin[24].add("difficultyTwoPressed", new Texture(Gdx.files.internal("versusAI/AIButton2selected.png")));
 
 
     }
