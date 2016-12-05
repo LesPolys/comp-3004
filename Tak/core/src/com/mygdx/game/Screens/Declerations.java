82dd483b3d81434687cfd2d483198db1f2e90ff6
BenJCIII <bencendanaiii@gmail.com>
2016-12-05 09:26:13 -0500

    rought AI pieces are randomly placed at every turn

--- Tak/core/src/com/mygdx/game/Screens/Declerations.java
+++ Tak/core/src/com/mygdx/game/Screens/Declerations.java
@@ -237,7 +237,6 @@ public class Declerations
 
 
 
-
     }
 
     //initalize the versus screen items
@@ -286,10 +285,10 @@ public class Declerations
 
     public void initVersusAIScreen()
     {
-        versusAISkin = new Skin[25];
-        versusAIMenutextButtonStyles = new TextButton.TextButtonStyle[25];
+        versusAISkin = new Skin[20];
+        versusAIMenutextButtonStyles = new TextButton.TextButtonStyle[20];
 
-        for(int i=0; i<=24; i++)
+        for(int i=0; i<=19; i++)
         {
             versusAISkin[i] = new Skin();
             versusAIMenutextButtonStyles[i] = new TextButton.TextButtonStyle();
@@ -316,12 +315,6 @@ public class Declerations
         versusAISkin[17].add("background", new Texture(Gdx.files.internal("mainMenu/Background2.png")));
         versusAISkin[18].add("versusAITitle", new Texture(Gdx.files.internal("versusAI/titleAI.png")));
         versusAISkin[19].add("versusMessage", new Texture(Gdx.files.internal("versusScreen/versusMessage.png")));
-        versusAISkin[20].add("difficultyMessage", new Texture(Gdx.files.internal("versusAI/difficultyMessage.png")));
-        versusAISkin[21].add("difficultyOneUnpressed", new Texture(Gdx.files.internal("versusAI/AIButton1.png")));
-        versusAISkin[22].add("difficultyOnePressed", new Texture(Gdx.files.internal("versusAI/AIButton1selected.png")));
-        versusAISkin[23].add("difficultyTwoUnpressed", new Texture(Gdx.files.internal("versusAI/AIButton2.png")));
-        versusAISkin[24].add("difficultyTwoPressed", new Texture(Gdx.files.internal("versusAI/AIButton2selected.png")));
-
 
     }
 
